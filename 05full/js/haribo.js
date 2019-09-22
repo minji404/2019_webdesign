6e79a2968591aa87b9e78a67f35122b8c3b047e9
minji404 <49858756+minji404@users.noreply.github.com>
2019-09-23 05:36:38 +0900

    study

--- 05full/js/haribo.js
+++ 05full/js/haribo.js
@@ -39,36 +39,44 @@ $(document).ready(function(){
 				}, function(){
 					$(this).find(".sub-m").stop().slideUp();
 				});
-// elements
-const text = document.getElementById('text');
-const input = document.getElementById('input');
+			$("#procon>.prod1>.jelly1,.jelly2").click(function(e){
+				e.preventDefault();
+				$(".popwrap").show();
+			});
+			$(".popc").click(function(e){
+				e.preventDefault();
+				$(".popwrap").hide();
+			});
+// // elements
+// const text = document.getElementById('text');
+// const input = document.getElementById('input');
 
-// add spans
-function addSpans(el, text) {
-    let letters = text.split('');
-    let html = '';
-    for (var i = 0; i < letters.length; i++) { 
-        html += '<span>' + letters[i] + '</span>';
-    }
-    el.innerHTML = html;
-}
-addSpans(text, text.innerText);
+// // add spans
+// function addSpans(el, text) {
+//     let letters = text.split('');
+//     let html = '';
+//     for (var i = 0; i < letters.length; i++) { 
+//         html += '<span>' + letters[i] + '</span>';
+//     }
+//     el.innerHTML = html;
+// }
+// addSpans(text, text.innerText);
 
-// setup timeline
-const tl = new TimelineLite();
-tl.staggerFromTo("#text span", 1.7, {
-    y: -window.innerHeight / 2 - 100,
-    x: -window.innerHeight / 2 - 100,
-}, {
-    y: 0,
-    x: 0,
-    ease: Bounce.easeOut,
-}, 0.03, "stagger"); 
+// // setup timeline
+// const tl = new TimelineLite();
+// tl.staggerFromTo("#text span", 1.7, {
+//     y: -window.innerHeight / 2 - 100,
+//     x: -window.innerHeight / 2 - 100,
+// }, {
+//     y: 0,
+//     x: 0,
+//     ease: Bounce.easeOut,
+// }, 0.03, "stagger"); 
 
-window.addEventListener('click', () => {
-     tl.restart();
-});
+// window.addEventListener('click', () => {
+//      tl.restart();
+// });
 
-tl.play();
+// tl.play();
 			
 		});
